5139dbe1cd4d0b523bf6585e38e6dc18df45caab
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-06-13 14:29:55 -0300

    fazendo o tratamento dos erros

--- lib/authentication/services/auth_service.dart
+++ lib/authentication/services/auth_service.dart
@@ -7,16 +7,27 @@ class AuthService {
     print("Entrando com usuário");
   }
 
-  cadastrarUsuario({
+  Future<String?> cadastrarUsuario({
     required String email,
     required String senha,
     required String nome,
   }) async {
-    UserCredential userCredential = await _firebaseAuth
-        .createUserWithEmailAndPassword(email: email, password: senha);
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: senha);
 
-    await userCredential.user!.updateDisplayName(nome);
+      await userCredential.user!.updateDisplayName(nome);
 
-    print("Funcionou chegamos até essa linha!");
+      print("Funcionou chegamos até essa linha!");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'email-already-in-use':
+          return 'O e-mail já está em uso.';
+      }
+
+      return e.code;
+    }
+
+    return null;
   }
 }
